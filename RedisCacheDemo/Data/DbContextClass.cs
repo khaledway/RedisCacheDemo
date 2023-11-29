9e65a8a1b64991851984239e895d97b9250d9581
Khaled <khaled.hussein.dev@gmail.com>
2023-11-29 11:26:59 +0200

    Add project files.

---
+++ RedisCacheDemo/Data/DbContextClass.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedisCacheDemo.Model;
+
+namespace RedisCacheDemo.Data
+{
+    public class DbContextClass: DbContext
+    {
+        public DbContextClass(DbContextOptions<DbContextClass> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
